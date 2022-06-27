504b9583582bad943e518d3d20a90a3cfe94295e
WML <mlwdesign2020@gmail.com>
2022-06-27 15:34:28 +0800

    feat:01.新增guide內容/02.新增『vue.config.js』路徑

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // 關閉eslint檢查
   lintOnSave: false, 
+  // build後須將dist放到伺服器啟動時需設定以下路徑
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/test/dist'
+  //   : '/'
 })
